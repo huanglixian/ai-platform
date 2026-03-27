e26bbfd13161071e20319e9e176b1bc3c088d748
LienMacM2 <lienshine@163.com>
2026-03-27 11:19:11 +0800

    删掉参考

--- reference/nanobot_webui/static/app.js
+++
@@ -1,7 +0,0 @@
-import { initChatComposer } from "./js/chat.js";
-import { initSecurityForm } from "./js/security.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  initChatComposer();
-  initSecurityForm();
-});
